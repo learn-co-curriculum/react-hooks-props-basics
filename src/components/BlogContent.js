6280343db50b2036c7190c1691e51a5a67acacb9
Emmanuel Kipsang <emmqnuelkipsqng@gmail.com>
2023-07-17 10:58:55 -0700

    practice props

--- src/components/BlogContent.js
+++ src/components/BlogContent.js
@@ -1,7 +1,20 @@
 import React from "react";
 
 function BlogContent(props) {
-  return <div id="blog-content">{props.articleText}</div>;
+  console.log(props);
+  if (!props.isPublished) {
+    //hide unpublished content
+    //return null means "don't display any DOM elements here"
+    return null;
+  }else {
+    //show published content
+    return (
+      <div>
+        <h1>{props.articleText}</h1>
+        <p>{props.minutesToRead} minutes to read</p>
+      </div>
+    )
+  }
 }
 
 export default BlogContent;
--- src/components/BlogPost.js
+++ src/components/BlogPost.js
@@ -5,8 +5,12 @@ import Comment from "./Comment";
 function BlogPost() {
   return (
     <div id="blog-post">
-      <BlogContent articleText="Dear Reader: Bjarne Stroustrup has the perfect lecture oration." />
-      <Comment commentText="I agree with this statement. - Angela Merkel" />
+      <BlogContent 
+        articleText="Dear Reader: Bjarne Stroustrup has the perfect lecture oration." 
+        isPublished={true}
+        minutesToRead={1}
+      />
+      <Comment commentText="I agree with this statement. - Emmanuel" />
       <Comment commentText="A universal truth. - Noam Chomsky" />
       <Comment commentText="Truth is singular. Its ‘versions’ are mistruths. - Sonmi-451" />
     </div>
